d0498ef9949ffb5c68681790d575880bd5a3eaaf
Aykhan <78858146+aykhan026@users.noreply.github.com>
2021-03-29 14:21:01 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "1566820396:AAEfeS0rZKhqDtfWVxe561TZ2MtEYKyMfAQ"
+TOKEN = "BotunTokenKodunuBuraYazın"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
